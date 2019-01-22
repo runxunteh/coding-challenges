7d30797fb85989c3b579a998ff5bb371f070c651
Run Xun <46264359+runxunteh@users.noreply.github.com>
2019-01-22 21:20:36 +0800

    Create test_QuestionsMarks.py
    A simple unit test file for Questions Marks program.

---
+++ test_QuestionsMarks.py
@@ -0,0 +1,10 @@
+import unittest
+from QuestionsMarks import QuestionsMarks
+
+class Test(unittest.TestCase):
+
+    def test_questionsmarks(self):
+        self.assertTrue("acc?7??sss?3rr1??????5")
+
+if __name__ == '__main__':
+    unittest.main()

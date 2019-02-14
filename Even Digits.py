6990f15863a06d2d7925df884a6780558865bc00
Run Xun <46264359+runxunteh@users.noreply.github.com>
2019-02-14 22:18:18 +0800

    Create Even Digits.py
    A program to determine the minimum number of button presses to make the calculator display an integer with no odd digits

---
+++ Even Digits.py
@@ -0,0 +1,54 @@
+"""
+Author: Teh Run Xun
+Date: 14 February 2019
+.......................
+This program is to determine the minimum number of button presses
+to make the calculator display an integer with no odd digits
+Example:
+Input: 42   Output: 0   #No odd digits
+Input: 2018 Output: 2   #pressing plus button twice to display 2020
+
+Input: 5327
+First odd digit is 5. Either decrement by 1 and replace all digits behind with 8
+Or increment by 1 and replace all digits behind with 0
+So, either 4888 or 6000
+5327-4888=439
+6000-5327=673
+So we choose the minimum number of button presses which is minus 439 times.
+"""
+
+def is_odd(x):
+    x=int(x)
+    if x%2!=0:
+        return True
+    return False
+
+def plus(x,i):
+    #if the first digit is 9 or all the digits to the left of 9 is 8
+    #e.g. 88892 and 91112
+    if int(x[i])==9:
+        if i==0 or x[0:i]=="8"*i:
+            new=str("20")+str("0"*(int(len(x))-1))
+    else:
+        new=str(x[0:i])+str(int(x[i])+1)+str("0"*int(len(x)-i-1))
+    return int(new)-int(x)
+
+def minus(x,i):
+    new=str(x[0:i])+str(int(x[i])-1)+str("8"*int(len(x)-i-1))
+    return int(x)-int(new)
+
+def even_digits(x):
+    x=str(x)
+    for i in range(len(x)):
+        if is_odd(x[i])==True:
+            y=plus(x,i)
+            z=minus(x,i)
+            min_button=min(y,z)
+            return min_button
+    return 0
+
+no_test_case=int(input())
+for i in range(no_test_case):
+    x=int(input())
+    print("Case #%d: %d"%(i+1,even_digits(x)))
+    

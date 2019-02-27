65418ed08608da83f6b1c56ab5fb77d66fd0dd76
Run Xun <46264359+runxunteh@users.noreply.github.com>
2019-02-27 20:13:40 +0800

    Create CHEFARRB.py
    This program is to find out how many sub-arrays of array A are good.

---
+++ CodeChef/Easy/CHEFARRB.py
@@ -0,0 +1,55 @@
+"""
+Author: Teh Run Xun
+Date: 19 February 2019
+https://www.codechef.com/problems/CHEFA
+.......................
+Chef has an array A consisting of N integers.
+A sub-array of the array is good if the bitwise OR of all the elements in it
+is greater or equal than number K.
+This program is to find out how many sub-arrays of array A are good.
+Bitwise OR: takes two bit patterns of equal length and performs the logical
+            inclusive OR operation on each pair of corresponding bits.
+            0101 4+3=7
+            0011
+            0111  The result in each position is 0 if both bits are 0,
+                  otherwise 1.
+
+Example:
+N K
+3 3
+1 2 3
+Case #1: 4 ({3},{1,2},{2,3},{1,2,3})
+Subarray: {1},{2},{3},{1,2},{2,3},{1,2,3}
+
+3 6
+3 4 5
+Case #2: 2 ({3,4},{3,4,5})
+Subarray: {3},{4},{5},{3,4},{4,5},{3,4,5}
+
+4 5
+5 6 4 3
+Case #3:  8 ({5},{6},{5,6},{6,4},{4,3},{5,6,4},{6,4,3},{5,6,4,3})
+Subarray: {5},{6},{4},{3},{5,6},{6,4},{4,3},{5,6,4},{6,4,3},{5,6,4,3}
+"""
+
+no_of_test=int(input())
+for i in range(no_of_test):
+    N,K=map(int,input().split())
+    A=list(map(int,input().split()))
+    x=0
+    forall=0
+    output=0
+    for i in range(N-1):
+        own=0
+        own=own|A[i]   #|: bitwise OR
+        if own>=K:
+            output+=1
+        
+        x=A[i]|A[i+1]
+        if x>=K:
+            output+=1
+        forall=forall|A[i]
+    forall=forall|A[N-1]
+    if forall>=K:
+        output+=1
+    print(output)
